54472aba18eec36c55d25d4e1019124708511f8a
Praveen <ppraveen25@gmail.com>
2015-05-26 22:08:48 -0400

    changed devise ERBs to HAMLs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true, length: {maxium: 25}
 end
