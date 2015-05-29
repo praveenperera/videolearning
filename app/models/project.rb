0acf31326e64ec6e1e59e0a6ed29074800c96c5d
Praveen <ppraveen25@gmail.com>
2015-05-29 17:02:14 -0400

    Added project and task controllers and views, styled project views

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
 	has_many :tasks
 
 	validates :name, presence: true, length: {maximum: 50}
-	validates :content, presence: true, length: {maximum: 500}
+	validates :content, presence: true
 	validates :price, presence: true, numericality: {only_integer: true}
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}, :controllers => {:omniauth_callbacks => "omniauth_callbacks"}
   get 'pages/about'
 
-  root to: "pages#about"
+  root to: "project#index"
+
+  resources :project do
+    resources :task, only: [:show]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
